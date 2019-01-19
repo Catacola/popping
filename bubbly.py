31436b54f7c525dfb25c4dc86fff936bd14f5eea
Anthony Kesich <akesich+github@gmail.com>
2019-01-19 14:11:09 -0800

    added data for problems 1 and 2

--- bubbly.py
+++ bubbly.py
@@ -1,15 +1,52 @@
+# bubble_chain
 # the key is a bubble and the value is the bubble it is inside
 # topmost bubbles are not in this map
+#
+# bubbles: all of the bubbles
 
 # example problem
-bubble_chain = {
+sample_chain = {
     'H': 'A',
     'A': 'E',
     'I': 'T',
 }
 
-# all off the bubbles
-bubbles = set('HAEITSN')
+sample_bubbles = set('HAEITSN')
+
+# problem 1
+chain1 = {
+    'A': 'R',
+    'R': 'C',
+    'G': 'C',
+    'H': 'C',
+    'S': 'H',
+    'U': 'S',
+    'I': 'U',
+    'T': 'S',
+    'V': 'S',
+    'E': 'V',
+    'L': 'E',
+}
+
+bubbles1 = set('NARCGHSUITVEL')
+
+#problem 2
+chain2 = {
+    'G': 'E',
+    'V': 'H',
+    'P': 'V',
+    'R': 'P',
+    'F': 'I',
+    'S': 'F',
+    'N': 'F',
+    'U': 'N',
+    'T': 'I',
+    'C': 'T',
+    'A': 'C',
+    'L': 'A',
+}
+
+bubbles2 = set('GEVPRHFSUNITALC')
 
 state = {}
 
@@ -65,11 +102,25 @@ def get_popped_bubbles(bubble, bubble_chain):
 def get_state_key(bubbles, my_turn):
     return (''.join(sorted(bubbles)), my_turn)
 
-winning_bubbles = []
+def winning_moves(bubbles, bubble_chain):
+    winning_bubbles = []
 
-for bubble in bubbles:
-    popped = get_popped_bubbles(bubble, bubble_chain)
-    if bubbly(bubbles - popped, bubble_chain, False):
-        winning_bubbles.append(bubble)
+    for bubble in bubbles:
+        popped = get_popped_bubbles(bubble, bubble_chain)
+        if bubbly(bubbles - popped, bubble_chain, False):
+            winning_bubbles.append(bubble)
 
-print(winning_bubbles)
+    return winning_bubbles
+
+print('Sample problem:')
+print(winning_moves(sample_bubbles, sample_chain))
+print()
+
+print('Problem 1:')
+print(winning_moves(bubbles1, chain1))
+print()
+
+print('Problem 2:')
+print(winning_moves(bubbles2, chain2))
+print()
+
